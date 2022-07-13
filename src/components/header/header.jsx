4d08e8eb04faac2718f99c5ef159797d124cc9c8
Ichouma <abdeljabarichouma@gmail.com>
2022-07-13 14:10:40 +0100

    commerce-ws

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,14 +5,12 @@ import "./header.css"
 
 function  Header() {
    function close(){
-    const close=document.getElementById('close');
     const nav=document.getElementById('navbar');
       nav.classList.remove("active");
 
   };
 
  function active(){
-    const bar=document.getElementById('bar');
     const nav=document.getElementById('navbar');
     const mobile=document.getElementById('mobile');
       nav.classList.add("active");
@@ -25,9 +23,9 @@ function  Header() {
          <a href="" className='logo' ><img  alt="" />A</a>
          <div>
           <ul id='navbar'>
-            <li > <a href="  go to #about"  >About</a></li>
-            <li > <a href=" go to #work">Portfolio</a></li>
-            <li > <a href="go to #contact">Conatct</a></li>
+            <li > <a href=""  >About</a></li>
+            <li > <a href="">Portfolio</a></li>
+            <li > <a href="">Conatct</a></li>
             <li  className='resume' > <a href="" className='resume'>Resume</a></li>
             <a href="" className='close' onClick={close} >
                 <hr className='hr1' /><hr className='hr2' />
